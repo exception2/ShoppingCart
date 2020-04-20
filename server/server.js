c3c8a24d0b7fac4e6117c4075069687989d7ce09
exception2 <kumarsatyendra727@gmail.com>
2020-04-20 21:50:09 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,11 +2,11 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, '../build');
+const publicPath = path.join(__dirname, '..','public');
 
 app.use(express.static(publicPath))
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build'), 'index.html')
+    res.sendFile(publicPath, 'index.html')
     res.end()
   })
 
